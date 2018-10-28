180ceaf5ccb8edede3ea58c76b2877c78487e7aa
Vipin Yadav <imvpn22@gmail.com>
2018-10-28 11:26:05 +0530

    minor changes

--- index.js
+++ index.js
@@ -65,24 +65,18 @@ fs.readFile('credentials.json', (err, content) => {
   });
 }
 
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
  function listMajors(auth) {
   const sheets = google.sheets({version: 'v4', auth});
   sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
+    spreadsheetId: '1m48qn4l172z3hBBJEE9JnnFI43eIyoeBT_Qi5oM34ME',
+    range: 'A1:E6',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     const rows = res.data.values;
     if (rows.length) {
       console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
       rows.map((row) => {
-        console.log(`${row[0]}, ${row[1]}, ${row[2]}, ${row[3]}, ${row[4]}`);
+        console.log(`${row[0]}, ${row[1]}, ${row[2]}, ${row[3]}`);
       });
     } else {
       console.log('No data found.');
