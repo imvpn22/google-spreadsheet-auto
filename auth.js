708a1027a71c9f165412977e7ceb9076497fc2bf
Vipin Yadav <imvpn22@gmail.com>
2018-10-29 23:45:17 +0530

    removed unnecessary files

--- auth.js
+++ auth.js
@@ -1,33 +1,34 @@
-module.exports.transporter_credentials = {             
+module.exports.transporter_credentials = {
     service: 'gmail',
     auth: {
-      user: 'yourmail@gmail.com', // place your@mail here
-      pass: 'your@gmailpassword'  // your@password
+      user: 'vpnydv123@gmail.com', // place your@mail here
+      pass: '<put your password here>'  // your@password
     }
   };
 
 module.exports.spreadsheet_credentials  = {
-    spreadsheetId: '1m48qn4l172z3hBBJEE9JnnFI43eIyoeBT_Qi5oM34ME',    //1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms
-    //range: 'Class Data!A1:G4',
+    // 1m48qn4l172z3hBBJEE9JnnFI43eIyoeBT_Qi5oM34ME
+    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',    //1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms
     valueRenderOption: 'FORMATTED_VALUE',
     // changes range according to your need
-    range: 'events!A2:K200'   //1m48qn4l172z3hBBJEE9JnnFI43eIyoeBT_Qi5oM34ME
-  }  
+    // range: 'events!A2:K200'
+    range: 'Class Data!A1:G4'
+  }
 
           // setup email data with unicode symbols
 module.exports.mailOptions  = {
-  from: 'amitykaran74@gmail.com', // sender address
-  to: 'amitykaran74@gmail.com', // list of receivers
+  from: 'vpnydv123@gmail.com', // sender address
+  to: 'vpnydv10yar@gmail.com', // list of receivers
   subject: 'Testing Mail', // Subject line
-  text: `Hi,\n This is Amit Yadav. 
-  You got this mail because you give me the assignment in which i have to do 
+  text: `Hi,\n This is Vipin Yadav.
+  You got this mail because you give me the assignment in which i have to do
   just access the data from "google sheets" and send it by mail on a interval.
-   I completed the given task as per instruction, if you want some improvement 
-   or changes just let me know. \nRegards \nAmit Yadav`, // plain text body
+   I completed the given task as per instruction, if you want some improvement
+   or changes just let me know. \nRegards \nVipin Yadav`, // plain text body
   attachments: [
-    {   
+    {
       filename: 'mydata.txt',
-      path: './data.txt' // stream this file
+      path: './data.json' // stream this file
   },
   ]
-};  
+};
