3ce09fe507de8bd19edd88452a71163131f2305f
Vipin Yadav <imvpn22@gmail.com>
2018-10-30 00:52:48 +0530

    Patch 1

--- auth.js
+++ auth.js
@@ -1,34 +1,35 @@
 module.exports.transporter_credentials = {
-    service: 'gmail',
-    auth: {
-      user: 'vpnydv123@gmail.com', // place your@mail here
-      pass: ''  // your@password
-    }
-  };
+  service: 'gmail',  // Name of the mail service
+  auth: {
+    user: 'vpnydv123@gmail.com', // place your@mail here
+    pass: '<Your password here>'  // your password
+  }
+};
 
 module.exports.spreadsheet_credentials  = {
-    // 1m48qn4l172z3hBBJEE9JnnFI43eIyoeBT_Qi5oM34ME
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',    //1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms
-    valueRenderOption: 'FORMATTED_VALUE',
-    // changes range according to your need
-    // range: 'events!A2:K200'
-    range: 'Class Data!A1:G4'
-  }
+  // 1m48qn4l172z3hBBJEE9JnnFI43eIyoeBT_Qi5oM34ME
+  spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms', // ID of Google Spreadsheet
+  valueRenderOption: 'FORMATTED_VALUE',
+  // changes range according to your need
+  // range: 'events!A2:K200'
+  range: 'Class Data!A1:F6'
+};
 
-          // setup email data with unicode symbols
+// setup email data with unicode symbols
 module.exports.mailOptions  = {
   from: 'vpnydv123@gmail.com', // sender address
   to: 'vpnydv10year@gmail.com', // list of receivers
   subject: 'Testing Mail', // Subject line
   text: `Hi,\n This is Vipin Yadav.
-  You got this mail because you give me the assignment in which i have to do
-  just access the data from "google sheets" and send it by mail on a interval.
-   I completed the given task as per instruction, if you want some improvement
-   or changes just let me know. \nRegards \nVipin Yadav`, // plain text body
-  attachments: [
-    {
-      filename: 'data.json',
-      path: './data.json' // stream this file
-  },
-  ]
+    You got this mail because you have choosed our service
+    for taking dump from your "Google Sheets" and send it
+    to you on a interval.
+
+    Please find the data file in attachments.
+
+    \nRegards \nVipin Yadav`,
+  attachments: [{
+    filename: 'data.json',
+    path: './data.json'
+  }]
 };
--- server.js
+++ server.js
@@ -9,123 +9,121 @@ const {google} = require('googleapis');
 const schedule = require('node-schedule');
 const nodemailer = require('nodemailer');
 const {transporter_credentials,
-  spreadsheet_credentials,
-  mailOptions  } = require('./auth');
+	spreadsheet_credentials,
+	mailOptions  } = require('./auth');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 const TOKEN_PATH = 'token.json';
 
-const app = express();
-const port = 5000;
-
-
-
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  authorize(JSON.parse(content), listMajors);
+	if (err) return console.log('Error loading client secret file:', err);
+	authorize(JSON.parse(content), listMajors);
 });
 
 function authorize(credentials, callback) {
-  console.log(`Authorizing with User Credentials:`)
-  // console.log(credentials);
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id, client_secret, redirect_uris[0]);
+	console.log(`Authorizing with User Credentials:`)
+	// console.log(credentials);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+	const {client_secret, client_id, redirect_uris} = credentials.installed;
+	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, (err, token) => {
+		if (err) {
+			console.log('No token file.');
+			console.log(err);
+			return getNewToken(oAuth2Client, callback);
+		} else {
+			console.log('Found old token file!');
+			oAuth2Client.setCredentials(JSON.parse(token));
+			callback(oAuth2Client);
+		}
+	});
 }
 
 function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
-        // Store the token to disk for later program executions
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-          if (err) console.error(err);
-          console.log('Token stored to', TOKEN_PATH);
-        });
-        callback(oAuth2Client);
-      });
-  });
+	const authUrl = oAuth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES,
+	});
+	console.log('Authorize this app by visiting this url:', authUrl);
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	rl.question('Enter the code from that page here: ', (code) => {
+		rl.close();
+		oAuth2Client.getToken(code, (err, token) => {
+			if (err) return console.error('Error while trying to retrieve access token', err);
+			oAuth2Client.setCredentials(token);
+				// Store the token to disk for later program executions
+				fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+					if (err) console.error(err);
+					else console.log('Token stored to', TOKEN_PATH);
+				});
+				callback(oAuth2Client);
+			});
+	});
 }
 
 function listMajors(auth) {
-  //send mail after every minute:5 second
-  var j = schedule.scheduleJob('5 * * * * *', () => {
+	//send mail after every minute:5 second
+	var j = schedule.scheduleJob('5 * * * * *', () => {
 
-    console.log('this is from node scheduler');
-    const sheets = google.sheets({version: 'v4', auth});
-    sheets.spreadsheets.values.get(spreadsheet_credentials, (err, res) => {
-          if (err) return console.log('The API returned an error: ' + err);
-          const rows = res.data.values;
+		console.log('Started node scheduler');
+		const sheets = google.sheets({version: 'v4', auth});
+		sheets.spreadsheets.values.get(spreadsheet_credentials, (err, res) => {
+			if (err) return console.log('The Google Spreadsheet API returned an error: ' + err);
 
-          if (rows.length) {
-            //accessing the row in sheet
-            const xyz = rows.map((row) => {
-              // converting array to proper json
-              return {Id: row[0],
-                date: row[1],
-                event: row[2],
-                occasion: row[3],
-                venuetype: row[4],
-                packagetype: row[5],
-                city: row[6],
-                guests: row[7],
-                status: row[8],
-                quality: row[9],
-                budget:row[10]
-              };
-            });
-            //mailer(rows[0]);
-            //console.log(rows,xyz);
-            const qwe = JSON.stringify({data: xyz});
+			const rows = res.data.values;
+			if (rows.length) {
+				//accessing the row in sheet
+				const xyz = rows.map((row) => {
+					// converting array to proper json
+					return {
+						"Student Name": row[0],
+						"Gender": row[1],
+						"Class Level": row[2],
+						"Home State": row[3],
+						"Major": row[4],
+						"Extracurricular Activity": row[5]
+					};
+				});
 
-            fs.writeFile('data.json', qwe, function(err, data){
-              if (err) {
-                console.log(err);
-              } else{
-                console.log("Successfully Written to File :", data);
-              }
-          });
+				//mailer(rows[0]);
+				//console.log(rows,xyz);
 
+				const qwe = JSON.stringify({data: xyz});
 
-      nodemailer.createTestAccount((err, account) => {
-          // create reusable transporter object using the default SMTP transport
-          //console.log(transporter_credentials);
-          let transporter = nodemailer.createTransport(transporter_credentials);
+				fs.writeFile('data.json', qwe, (err, data) => {
+					if (err) {
+						console.log(err);
+					} else {
+						console.log("Successfully Written to File : data.json");
+					}
+				});
 
-          // send mail with defined transport object
-          transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-              // Preview only available when sending through an Ethereal account
-              console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+				nodemailer.createTestAccount((err, account) => {
+					// create reusable transporter object using the default SMTP transport
+					//console.log(transporter_credentials);
+					let transporter = nodemailer.createTransport(transporter_credentials);
 
-            });
-        });
-    } else {
-      console.log('No data found.');
-    }
-  });
-  });
+					// send mail with defined transport object
+					transporter.sendMail(mailOptions, (error, info) => {
+						if (error) {
+							return console.log(error);
+						} else {
+							console.log('Message sent: %s', info.messageId);
+							// Preview only available when sending through an Ethereal account
+							console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+						}
+					});
+				});
+			} else {
+				console.log('No data found.');
+			}
+		});
+	});
 }
